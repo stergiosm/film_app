cd0fd31ee13f70973c005b856ef2efbb3fca55fc
Stergios Michopoulos <stergios.michopoulos@gmail.com>
2016-03-23 08:41:36 +0100

    fix server

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 3000;
-app.use(express.static(__dirname + '/dev'));
-app.get('*', function(req, res){
-  res.redirect('/404.html');
-});
+
 app.listen(port);
 console.log('Listening on port ' + port);
